054118ed375041f473dd3b1155c23322e525ac96
Final4Head <jeffreymai3000@gmail.com>
2024-11-15 11:29:07 -0500

    optimized code

--- Analyzer.sh
+++ Analyzer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 updateData() {
-  echo "$name = $myScore" >> $1
-  echo "Opponent = $oppScore" >> $1
-  echo $(date) >> $1
-  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> $1
+  echo "$name = $myScore" >> "$(realpath "$1")"
+  echo "Opponent = $oppScore" >> "$(realpath "$1")"
+  echo $(date) >> "$(realpath "$1")"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> "$(realpath "$1")"
 }
 
 updateScoreboard() {
@@ -21,40 +21,16 @@ updateScoreboard() {
         players["$playerName"]=$playerScore
       fi
     fi
-  done < $1
+  done < "$(realpath "$1")"
 
-  echo "Total wins against opponent" | tee -a $2
+  echo "Total wins against opponent" | tee -a "$(realpath "$2")"
   for key in "${!players[@]}"; do
-    echo "$key: ${players[$key]}" | tee -a $2
+    echo "$key: ${players[$key]}" | tee -a "$(realpath "$2")"
   done
-  echo $(date) >> $2
-  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> $2
-}
-
-updateScoreboardCrontab() {
-  declare -A players
-  while IFS= read -r line; do
-    index=$(expr index "$line" "=")
-    if [ "$index" -gt 0 ]; then
-      playerName=${line:0:$((index-2))}
-      playerScore=${line:$((index))}
-
-      if [[ -v players["$playerName"] ]]; then
-        players["$playerName"]=$(( players["$playerName"] + $playerScore ))
-      elif [[ $playerName != "Opponent" ]]; then
-        players["$playerName"]=$playerScore
-      fi
-    fi
-  done < "$1"
-
-  echo "Total wins against opponent" | tee -a "$2"
-  for key in "${!players[@]}"; do
-    echo "$key: ${players[$key]}" | tee -a "$2"
-  done
-  echo $(date) >> "$2"
-  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> "$2"
+  echo $(date) >> "$(realpath "$2")"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> "$(realpath "$2")"
 }
 
 if [ "${1:0:5}" == "/mnt/" ]; then
-  updateScoreboardCrontab "/mnt/d/Visual Studio Codes/Revature/Week 1/Project/Data.md" "/mnt/d/Visual Studio Codes/Revature/Week 1/Project/Scoreboard.txt"
+  updateScoreboard "${1:-7}" "${2:-14}"
 fi
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Rock, Paper, Scissors
 Run `GameScript.sh` with bash and have fun!
-
-If you want to use crontab to update the scoreboard:
-- Go to `Analyzer.sh`
-- On line 59, you need to change the paths to your `Data.md` and `Scoreboard.txt` files
